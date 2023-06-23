143b5c3f97139cb9c20b86bc158d5302e9b64010
sp-22-11 <sp2022cq@proton.me>
2023-06-22 22:39:19 -0400

    completed client js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+
+
+
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
 async function makeReq(){
 
-  const userName = document.querySelector("#clickMe").value;
-  const res = await fetch(`/api?student=${userName}`)
+
+  const res = await fetch(`/api`)
   const data = await res.json()
 
-  console.log(data);
-  document.querySelector("#typeOfCuisine").textContent = data.name
+  let randomNum=Math.floor(Math.random()*10)+1
+  document.querySelector('#typeOfCuisine').textContent = data[randomNum].typeOfFood
+  document.getElementById('clickMe').style.display='none'
 }
