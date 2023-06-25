11c75525fde7ef9bfeb31dc8bc305d755aabc137
DJ Neill <dj.neill856@gmail.com>
2023-06-25 14:20:49 -0500

    remove error logs

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ document.querySelector('#clickMe').addEventListener('click', makeReq);
 async function makeReq() {
   const pick = document.querySelector("button[name=button]").value
   const res = await fetch(`https://what-to-eat-api.onrender.com/api${pick}`);
-  const responseText = await res.text();
-  console.log(responseText);
 
   try {
     const data = JSON.parse(responseText);
