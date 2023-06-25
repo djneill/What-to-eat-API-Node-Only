9c63fdbca3165d91c515724c305d7c5e88bdd76e
DJ Neill <dj.neill856@gmail.com>
2023-06-24 22:31:03 -0500

    add cors to fix cross origin error

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+document.querySelector('#clickMe').addEventListener('click', makeReq);
 
-async function makeReq(){
+async function makeReq() {
+  const res = await fetch(`https://what-to-eat-api-node-only.vercel.app/`);
+  const responseText = await res.text();
+  console.log(responseText);
 
-  const res = await fetch(`https://what-to-eat-api-node-only.vercel.app/`)
-  const data = await res.json()
-
-  console.log(data)
-  // let randomNum=Math.floor(Math.random()*10)+1
-  document.querySelector('#typeOfCuisine').textContent = data.typeOfFood
-  // document.getElementById('clickMe').style.display='none'
+  try {
+    const data = JSON.parse(responseText);
+    console.log(data);
+    document.querySelector('#typeOfCuisine').textContent = data.typeOfFood;
+  } catch (error) {
+    console.error('Failed to parse JSON:', error);
+  }
 }
